d4cecdb2d24d9aabe306db6770c0a25fa1937512
Raphaël Weis <raphael.weis.g@gmail.com>
2025-01-16 12:36:18 +0100

    feat(profile): Added full UI for profile information dialog.

--- app/src/main/java/com/raphaelweis/rcube/ui/destinations/profile/ProfileInformationDialogScreen.kt
+++ app/src/main/java/com/raphaelweis/rcube/ui/destinations/profile/ProfileInformationDialogScreen.kt
@@ -1,37 +1,114 @@
 package com.raphaelweis.rcube.ui.destinations.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.raphaelweis.rcube.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileInformationDialogScreen() {
+fun ProfileInformationDialogScreen(mainNavController: NavHostController) {
+    var username by remember { mutableStateOf("") }
+    var birthdate by remember { mutableStateOf("") }
+
     Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-        TopAppBar(title = { Text(stringResource(R.string.profile_information)) })
+        TopAppBar(navigationIcon = {
+            IconButton(onClick = { mainNavController.popBackStack() }) {
+                Icon(
+                    painter = painterResource(R.drawable.close_outlined),
+                    contentDescription = "hello",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }, actions = {
+            TextButton(
+                content = { Text(stringResource(R.string.save)) },
+                onClick = {},
+            )
+        }, title = { Text(stringResource(R.string.profile_information)) })
     }) { paddingValues ->
         Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier
                 .padding(paddingValues)
+                .padding(horizontal = 16.dp)
                 .fillMaxSize()
         ) {
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
-            Text("Hello")
+            Text(stringResource(R.string.profile_information_dialog_info))
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text(stringResource(R.string.username)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = birthdate,
+                singleLine = true,
+                onValueChange = { birthdate = it },
+                visualTransformation = DateTransformation(),
+                label = { Text(stringResource(R.string.date_of_birth)) },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
+}
+
+class DateTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        return dateFilter(text)
+    }
+}
+
+fun dateFilter(text: AnnotatedString): TransformedText {
+
+    val trimmed = if (text.text.length >= 8) text.text.substring(0..7) else text.text
+    var out = ""
+    for (i in trimmed.indices) {
+        out += trimmed[i]
+        if (i % 2 == 1 && i < 4) out += "/"
+    }
+
+    val numberOffsetTranslator = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            if (offset <= 1) return offset
+            if (offset <= 3) return offset + 1
+            if (offset <= 8) return offset + 2
+            return 10
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            if (offset <= 2) return offset
+            if (offset <= 5) return offset - 1
+            if (offset <= 10) return offset - 2
+            return 8
+        }
+    }
+
+    return TransformedText(AnnotatedString(out), numberOffsetTranslator)
 }
